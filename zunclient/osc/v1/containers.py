dc0b9b5e7be69f5e96f0e0570db58755db724777
Namrata Sitlani <sitlani.namrata@yahoo.in>
2016-12-22 18:10:21 +0000

    Add OSC Plugin for openstack appcontainer create
    This change implements the "openstack appcontainer create" command
     Based on existing zun command: zun create
    
    Partially Implements:blueprint zun-osc-plugin
    
    Change-Id: I1967745798c4ae84da8f33e318a41ee9714e3511

--- zunclient/osc/v1/containers.py
+++ zunclient/osc/v1/containers.py
@@ -15,6 +15,8 @@ import logging
 from osc_lib.command import command
 from osc_lib import utils
 
+from zunclient.common import utils as zun_utils
+
 
 def _container_columns(container):
     del container._info['links']
@@ -24,6 +26,94 @@ def _container_columns(container):
 def _get_client(obj, parsed_args):
     obj.log.debug("take_action(%s)" % parsed_args)
     return obj.app.client_manager.container
+
+
+class CreateContainer(command.ShowOne):
+    """Create a container"""
+
+    log = logging.getLogger(__name__ + ".CreateContainer")
+
+    def get_parser(self, prog_name):
+        parser = super(CreateContainer, self).get_parser(prog_name)
+        parser.add_argument(
+            '--name',
+            metavar='<name>',
+            help='name of the container')
+        parser.add_argument(
+            '--image',
+            required=True,
+            metavar='<image>',
+            help='name or ID of the image')
+        parser.add_argument(
+            '--command',
+            metavar='<command>',
+            help='Send command to the container')
+        parser.add_argument(
+            '--cpu',
+            metavar='<cpu>',
+            help='The memory of virtual cpus.')
+        parser.add_argument(
+            '--memory',
+            metavar='<memory>',
+            help='The container memory size (format: <number><optional unit>, '
+                 'where unit = b, k, m or g)')
+        parser.add_argument(
+            '--environment',
+            metavar='<KEY=VALUE>',
+            action='append', default=[],
+            help='The environment variabled')
+        parser.add_argument(
+            '--workdir',
+            metavar='<workdir>',
+            help='The working directory for commands to run in')
+        parser.add_argument(
+            '--expose',
+            metavar='<port>',
+            action='append', default=[],
+            help='A port or a list of ports to expose. '
+                 'May be used multiple times.')
+        parser.add_argument(
+            '--hostname',
+            metavar='<hostname>',
+            help='The hostname to use for the container')
+        parser.add_argument(
+            '--label',
+            metavar='<KEY=VALUE>',
+            action='append', default=[],
+            help='Adds a map of labels to a container. '
+                 'May be used multiple times.')
+        parser.add_argument(
+            '--image-pull-policy',
+            dest='image_pull_policy',
+            metavar='<policy>',
+            choices=['never', 'always', 'ifnotpresent'],
+            help='The policy which determines if the image should '
+                 'be pulled prior to starting the container. '
+                 'It can have following values: '
+                 '"ifnotpresent": only pull the image if it does not '
+                 'already exist on the node. '
+                 '"always": Always pull the image from repositery.'
+                 '"never": never pull the image')
+        return parser
+
+    def take_action(self, parsed_args):
+        client = _get_client(self, parsed_args)
+        opts = {}
+        opts['name'] = parsed_args.name
+        opts['image'] = parsed_args.image
+        opts['command'] = parsed_args.command
+        opts['memory'] = parsed_args.memory
+        opts['cpu'] = parsed_args.cpu
+        opts['environment'] = zun_utils.format_labels(parsed_args.environment)
+        opts['workdir'] = parsed_args.workdir
+        opts['ports'] = parsed_args.expose
+        opts['hostname'] = parsed_args.hostname
+        opts['labels'] = zun_utils.format_labels(parsed_args.label)
+        opts['image_pull_policy'] = parsed_args.image_pull_policy
+
+        container = client.containers.create(**opts)
+        columns = _container_columns(container)
+        return columns, utils.get_item_properties(container, columns)
 
 
 class ShowContainer(command.ShowOne):
