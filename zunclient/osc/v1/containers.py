5f460eeb95b63d868e18bac65389a68181b2f901
Feng Shengqin <feng.shengqin@zte.com.cn>
2017-03-28 19:20:40 +0800

    Support to kill multi-containers percommand by osc
    Change-Id: Ic0794a8e228c0070829021e23407f5b321ca4c1f

--- zunclient/osc/v1/containers.py
+++ zunclient/osc/v1/containers.py
@@ -430,16 +430,17 @@ class LogsContainer(command.Command):
 
 
 class KillContainer(command.Command):
-    """Kill signal to a container"""
+    """Kill specified containers"""
 
     log = logging.getLogger(__name__ + ".KillContainers")
 
     def get_parser(self, prog_name):
         parser = super(KillContainer, self).get_parser(prog_name)
         parser.add_argument(
-            'container',
+            'containers',
             metavar='<container>',
-            help='ID or name of the container to kill signal to.')
+            nargs='+',
+            help='ID or name of the (container)s to kill.')
         parser.add_argument(
             '--signal',
             metavar='<signal>',
@@ -449,18 +450,18 @@ class KillContainer(command.Command):
 
     def take_action(self, parsed_args):
         client = _get_client(self, parsed_args)
-        container = parsed_args.container
-        opts = {}
-        opts['id'] = parsed_args.container
-        opts['signal'] = parsed_args.signal
-        opts = zun_utils.remove_null_parms(**opts)
-        try:
-            client.containers.kill(**opts)
-            print(_('Request to send kill signal to container %s has '
-                    'been accepted') % container)
-        except Exception as e:
-            print("kill signal for container %(container)s failed: %(e)s" %
-                  {'container': container, 'e': e})
+        for container in parsed_args.containers:
+            opts = {}
+            opts['id'] = container
+            opts['signal'] = parsed_args.signal
+            opts = zun_utils.remove_null_parms(**opts)
+            try:
+                client.containers.kill(**opts)
+                print(_('Request to send kill signal to container %s has '
+                        'been accepted') % container)
+            except Exception as e:
+                print("kill signal for container %(container)s failed: %(e)s" %
+                      {'container': container, 'e': e})
 
 
 class StopContainer(command.Command):
