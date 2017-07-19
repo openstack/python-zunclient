071a047a25d1a4b2175cea0238d72ed0f22dbede
Hongbin Lu <hongbin.lu@huawei.com>
2017-07-19 15:09:20 +0000

    Fixed the api version issue on OSC plugin
    * Fixed an issue that the api version is passed with wrong keyword
      argument (should be 'api_version', not 'os_container_api_version').
    * Bump the api version to 1.2 which is the current latest.
    
    Closes-Bug: #1705283
    Change-Id: Idef09cce42003ccc113d6113640df4ab69f4b690

--- zunclient/tests/unit/osc/test_plugin.py
+++ zunclient/tests/unit/osc/test_plugin.py
@@ -18,16 +18,18 @@ from zunclient.tests.unit import base
 
 class TestContainerPlugin(base.TestCase):
 
+    @mock.patch("zunclient.api_versions.get_api_version")
     @mock.patch("zunclient.v1.client.Client")
-    def test_make_client(self, p_client):
+    def test_make_client(self, p_client, mock_get_api_version):
 
         instance = mock.Mock()
         instance._api_version = {"container": '1'}
         instance._region_name = 'zun_region'
         instance.session = 'zun_session'
+        mock_get_api_version.return_value = '1.2'
 
         plugin.make_client(instance)
-        p_client.assert_called_with(os_container_api_version='1',
-                                    region_name='zun_region',
+        p_client.assert_called_with(region_name='zun_region',
                                     session='zun_session',
-                                    service_type='container')
+                                    service_type='container',
+                                    api_version='1.2')
