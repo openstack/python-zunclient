29ac0b0077891f397c4b22328fe39575f52a8c23
pengdake <19921207pq@gmail.com>
2018-02-02 07:48:37 +0800

    Add image show for openstackclient
    Closes-Bug: #1751428
    
    Change-Id: Ibfeb0765fa9dc06ef78699dc91fbbf83f31c4ed5
    Signed-off-by: pengdake <19921207pq@gmail.com>

--- zunclient/osc/v1/images.py
+++ zunclient/osc/v1/images.py
@@ -119,3 +119,25 @@ class SearchImage(command.Lister):
         columns = ('ID', 'Name', 'Tags', 'Status', 'Size', 'Metadata')
         return (columns, (utils.get_item_properties(image, columns)
                           for image in images))
+
+
+class ShowImage(command.ShowOne):
+    """Describe a specific image"""
+
+    log = logging.getLogger(__name__ + ".ShowImage")
+
+    def get_parser(self, prog_name):
+        parser = super(ShowImage, self).get_parser(prog_name)
+        parser.add_argument(
+            'uuid',
+            metavar='<uuid>',
+            help='UUID of image to describe')
+        return parser
+
+    def take_action(self, parsed_args):
+        client = _get_client(self, parsed_args)
+        opts = {}
+        opts['id'] = parsed_args.uuid
+        image = client.images.get(**opts)
+        columns = _image_columns(image)
+        return columns, utils.get_item_properties(image, columns)
