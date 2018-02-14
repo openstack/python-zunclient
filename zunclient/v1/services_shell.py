e759493abcea96517e871f08b114a29976ae53f8
Hongbin Lu <hongbin034@gmail.com>
2018-02-14 04:00:01 +0000

    Don't display 'Created At' for service
    This field doesn't provide too much information. Also, both nova
    and cinder don't display this field so we remove it for consistency.
    
    Change-Id: Ia7968359862615bb38f9d1abb3f4a981c1329700

--- zunclient/v1/services_shell.py
+++ zunclient/v1/services_shell.py
@@ -21,7 +21,7 @@ def do_service_list(cs, args):
     """Print a list of zun services."""
     services = cs.services.list()
     columns = ('Id', 'Host', 'Binary', 'State', 'Disabled',
-               'Disabled Reason', 'Created At', 'Updated At')
+               'Disabled Reason', 'Updated At')
     utils.print_list(services, columns,
                      {'versions': zun_utils.print_list_field('versions')})
 
