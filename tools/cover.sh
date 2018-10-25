openstack/python-zunclient

commit 1c1d23c72361509f50dadfaab7d12b4911bf7213

Switch to using stestr
According to Openstack summit session [1] stestr is
maintained project to which all Openstack projects
should migrate.
Let's switch it then.

[1] https://etherpad.openstack.org/p/YVR-python-pti

Change-Id: I0b5b080be1fefc8de28f40a4afced16608964b2c
Signed-off-by: Charles Short <zulcss@gmail.com>

tox.ini
@@ -17,7 +17,8 @@ deps = -r{toxinidir}/requirements.txt
 
 commands =
   find . -type f -name "*.py[c|o]" -delete
-  bash tools/pretty_tox.sh '{posargs}'
+  stestr run {posargs}
+  stestr slowest
 
 
 [testenv:bandit]
@@ -80,7 +81,7 @@ exclude=.venv,.git,.tox,dist,doc,*lib/python*,*egg,build
 import_exceptions = zunclient._i18n
 
 [testenv:functional]
-commands = 
+commands =
     find . -type f -name "*.py[c|o]" -delete
     ostestr {posargs}
 setenv =

tools/pretty_tox.sh
@@ -1,16 +1,0 @@
-#!/usr/bin/env bash
-
-set -o pipefail
-
-TESTRARGS=$1
-
-# --until-failure is not compatible with --subunit see:
-#
-# https://bugs.launchpad.net/testrepository/+bug/1411804
-#
-# this work around exists until that is addressed
-if [[ "$TESTARGS" =~ "until-failure" ]]; then
-    python setup.py testr --slowest --testr-args="$TESTRARGS"
-else
-    python setup.py testr --slowest --testr-args="--subunit $TESTRARGS" | subunit-trace -f
-fi

commit cccaac953238add8b2aa51fd7643c1009c9c8c55

Fix the 'tox -e cover' command
The 'cover' environment broke after commit
1c1d23c72361509f50dadfaab7d12b4911bf7213 because it still use the
old 'testr' command (but we have switched to stestr).

Change-Id: I782f895329f64368c6fde3470a12514bbe563d87

tools/cover.sh
@@ -1,69 +1,0 @@
-#!/bin/bash
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-
-ALLOWED_EXTRA_MISSING=0
-
-show_diff () {
-    head -1 $1
-    diff -U 0 $1 $2 | sed 1,2d
-}
-
-if ! git diff --exit-code || ! git diff --cached --exit-code
-then
-    echo "There are uncommitted changes!"
-    echo "Please clean git working directory and try again"
-    exit 1
-fi
-
-# Checkout master and save coverage report
-git checkout HEAD^
-
-baseline_report=$(mktemp -t zunclient_coverageXXXXXXX)
-find . -type f -name "*.pyc" -delete && python setup.py testr --coverage --testr-args="$*"
-coverage report > $baseline_report
-mv cover cover-master
-cat $baseline_report
-baseline_missing=$(awk 'END { print $3 }' $baseline_report)
-
-# Checkout back and save coverage report
-git checkout -
-
-current_report=$(mktemp -t zunclient_coverageXXXXXXX)
-find . -type f -name "*.pyc" -delete && python setup.py testr --coverage --testr-args="$*"
-coverage report > $current_report
-current_missing=$(awk 'END { print $3 }' $current_report)
-
-# Show coverage details
-allowed_missing=$((baseline_missing+ALLOWED_EXTRA_MISSING))
-
-echo "Allowed to introduce missing lines : ${ALLOWED_EXTRA_MISSING}"
-echo "Missing lines in master            : ${baseline_missing}"
-echo "Missing lines in proposed change   : ${current_missing}"
-
-if [ $allowed_missing -ge $current_missing ]; then
-    if [ $baseline_missing -lt $current_missing ]; then
-        show_diff $baseline_report $current_report
-        echo "We believe you can test your code with 100% coverage!"
-    else
-        echo "Thank you! You are awesome! Keep writing unit tests! :)"
-    fi
-    exit_code=0
-else
-    show_diff $baseline_report $current_report
-    echo "Please write more unit tests, we must maintain our test coverage :( "
-    exit_code=1
-fi
-
-rm $baseline_report $current_report
-exit $exit_code

tox.ini
@@ -75,7 +75,15 @@ commands = {posargs}
 
 [testenv:cover]
 basepython = python3
-commands = {toxinidir}/tools/cover.sh {posargs}
+setenv =
+    {[testenv]setenv}
+    PYTHON=coverage run --source zunclient --parallel-mode
+commands =
+    stestr run {posargs}
+    coverage combine
+    coverage html -d cover
+    coverage xml -o cover/coverage.xml
+    coverage report
 
 [flake8]
 # E123, E125 skipped as they are invalid PEP-8.