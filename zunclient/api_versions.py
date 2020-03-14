3afaa59f749ca9127d7f462a04c10bdc68f9855e
Hongbin Lu <hongbin034@gmail.com>
2020-03-14 20:47:36 +0000

    Bump API version to 1.38
    API version is introduced by: https://review.opendev.org/#/c/700284/.
    This patch bump the API version accordingly.
    
    Change-Id: I6fbed16e8b66f7aef5fa65c6462e977699aeeb96

--- zunclient/api_versions.py
+++ zunclient/api_versions.py
@@ -31,7 +31,7 @@ if not LOG.handlers:
 HEADER_NAME = "OpenStack-API-Version"
 SERVICE_TYPE = "container"
 MIN_API_VERSION = '1.1'
-MAX_API_VERSION = '1.37'
+MAX_API_VERSION = '1.38'
 DEFAULT_API_VERSION = '1.latest'
 
 _SUBSTITUTIONS = {}
